89a501d3fc75d5aa3e0315c1c4bd703b63407ecc
Will Sutton <wsutton17@gmail.com>
2021-02-11 09:06:55 -0500

    db down

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const TweetModel = require('./db/tweet')
+// const TweetModel = require('./db/tweet')
 const port = process.env.PORT || 4000
 
 app.use(cors())
